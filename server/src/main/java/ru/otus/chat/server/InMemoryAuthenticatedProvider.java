e966b1cbc561d82b9cc016dd057fff4b9ed5c9b8
Aleksey <kot543212@mail.ru>
2025-06-19 19:07:46 +0300

    init commit

---
+++ server/src/main/java/ru/otus/chat/server/InMemoryAuthenticatedProvider.java
@@ -0,0 +1,107 @@
+package ru.otus.chat.server;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class InMemoryAuthenticatedProvider implements AuthenticatedProvider {
+    private class User {
+        private String login;
+        private String password;
+        private String username;
+
+        public User(String login, String password, String username) {
+            this.login = login;
+            this.password = password;
+            this.username = username;
+        }
+    }
+
+    private List<User> users;
+    private Server server;
+
+    public InMemoryAuthenticatedProvider(Server server) {
+        this.server = server;
+        this.users = new CopyOnWriteArrayList<>();
+        this.users.add(new User("qwe", "qwe", "qwe1"));
+        this.users.add(new User("asd", "asd", "asd1"));
+        this.users.add(new User("zxc", "zxc", "zxc1"));
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("Сервис аунтентификации запущен: InMemory режим");
+    }
+
+    private String getUsernameByLoginAndPassword(String login, String password) {
+        for (User user : users) {
+            if (user.login.equals(login.toLowerCase()) && user.password.equals(password)) {
+                return user.username;
+            }
+        }
+        return null;
+    }
+
+    private boolean isLoginAlreadyExists(String login) {
+        for (User user : users) {
+            if (user.login.equals(login.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isUsernameAlreadyExists(String username) {
+        for (User user : users) {
+            if (user.username.equalsIgnoreCase(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean authenticate(ClientHandler clientHandler, String login, String password) {
+        String authUsername = getUsernameByLoginAndPassword(login, password);
+        if (authUsername == null) {
+            clientHandler.sendMsg("Некорректный логин/пароль");
+            return false;
+        }
+        if (server.isUsernameBusy(authUsername)) {
+            clientHandler.sendMsg("Указанная учетная запись уже занята");
+            return false;
+        }
+        clientHandler.setUsername(authUsername);
+        server.subscribe(clientHandler);
+        clientHandler.sendMsg("/authok " + authUsername);
+        return true;
+    }
+
+    @Override
+    public boolean registration(ClientHandler clientHandler, String login, String password, String username) {
+        if (login.length() < 3) {
+            clientHandler.sendMsg("Логин должен быть 3+ символа");
+            return false;
+        }
+        if (username.length() < 3) {
+            clientHandler.sendMsg("Имя пользователя должна быть 3+ символа");
+            return false;
+        }
+        if (password.length() < 3) {
+            clientHandler.sendMsg("Пароль должен быть 3+ символа");
+            return false;
+        }
+        if (isLoginAlreadyExists(login)) {
+            clientHandler.sendMsg("Такой логин уже занят");
+            return false;
+        }
+        if (isUsernameAlreadyExists(username)) {
+            clientHandler.sendMsg("Такое имя пользователя уже занято");
+            return false;
+        }
+        users.add(new User(login, password, username));
+        clientHandler.setUsername(username);
+        server.subscribe(clientHandler);
+        clientHandler.sendMsg("/regok " + username);
+        return true;
+    }
+}
---
+++ server/src/main/java/ru/otus/chat/server/Server.java
@@ -0,0 +1,60 @@
+package ru.otus.chat.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Server {
+    private int port;
+    private List<ClientHandler> clients;
+    private AuthenticatedProvider authenticatedProvider;
+
+    public Server(int port) {
+        this.port = port;
+        clients = new CopyOnWriteArrayList<>();
+        authenticatedProvider = new InMemoryAuthenticatedProvider(this);
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен на порту " + port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ClientHandler(socket, this);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        broadcastMessage("Клиент " + clientHandler.getUsername() + " вышел из чата");
+        System.out.println("Клиент " + clientHandler.getUsername() + " вышел из чата");
+        clients.remove(clientHandler);
+    }
+
+    public void broadcastMessage(String message) {
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public boolean isUsernameBusy(String username) {
+        for (ClientHandler c : clients) {
+            if (c.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public AuthenticatedProvider getAuthenticatedProvider() {
+        return authenticatedProvider;
+    }
+}
